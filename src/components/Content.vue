8a9a4c1b71e08433a8be6ddfffbcce4df68a897f
yakuptokmakci <yakuptokmakci71@gmail.com>
2024-03-10 22:46:13 +0300

    fifth commit card text is done

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,9 +1,16 @@
 <template>
   <h2>NBA Statistics</h2>
-  <v-card :text="main_idea" variant="outlined"></v-card>
+  <v-card
+    variant="outlined"
+    image="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+  >
+    <template #title>{{ title }}</template>
+    <template #text>{{ main_idea }}</template>
+  </v-card>
 </template>
 
 <script setup>
+const title = "Scope Of the Project";
 const main_idea =
-  "The main idea behind of this project is to compare stats between two palyer over more than 500 players";
+  "The central objective of this project is to compare the statistics of two players  As you may be aware, the NBA has a committee primarily focused on statistics. Therefore, our application serves as the optimal solution to fulfill this interest. Our app can furnish comprehensive statistics for over 700 players in the latest NBA season";
 </script>
