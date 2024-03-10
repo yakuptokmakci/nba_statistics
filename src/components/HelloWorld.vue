98d0bcef42b4b9ba6f82000a0f24d5001e7f510e
yakuptokmakci <yakuptokmakci71@gmail.com>
2024-03-10 19:36:34 +0300

    ilk commit yapıldı

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-data-table
+    :items="items"
+    loading-text="Loading... Please wait"
+    height="400"
+    item-value="name"
+  ></v-data-table>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const items = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("src/assets/player_stats.json");
+    items.value = response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
+</script>
