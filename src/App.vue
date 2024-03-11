b2dce94ec6cb2b26a66d8a12c29e9d1f037d4147
yakuptokmakci <yakuptokmakci71@gmail.com>
2024-03-12 00:08:00 +0300

    compare components logis is done nex time for prop drilling

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,29 @@
         </v-main>
       </v-app>
     </div>
-    <div class="row"></div>
+    <div class="row">
+      <Compare :players="filteredData" />
+    </div>
   </div>
 </template>
 
-<script setup>
+<script>
+import axios from "axios";
+import { onMounted } from "vue";
 import ContentTable from "./components/ContentTable.vue";
+import Compare from "./components/Compare.vue";
 
-//
+export default {
+  setup() {
+    onMounted(async () => {
+      try {
+        const response = await axios.get("src/assets/player_stats.json");
+        const filteredData = response.data.map((item) => item.Rk);
+        console.log(filteredData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    });
+  },
+};
 </script>
--- src/components/Compare.vue
+++ src/components/Compare.vue
@@ -1,3 +1,42 @@
-<template></template>
+<template>
+  <div class="container">
+    <v-btn color="black" @click="handleClick" v-if="isCompare"
+      >Compare The Players</v-btn
+    >
+    <div class="row" v-if="!isCompare">
+      <div class="col">
+        <button
+          class="btn btn-secondary btn-lg dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Slecet Player 1
+        </button>
+      </div>
+      <div class="col">
+        <button
+          class="btn btn-secondary btn-lg dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Slecet Player 2
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(["players"]);
+
+const isCompare = ref(true);
+
+const handleClick = () => {
+  isCompare.value = !isCompare.value;
+  console.log(isCompare.value);
+};
+</script>
