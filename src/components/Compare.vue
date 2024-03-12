38715cce8704217795e9186c9fcc8b0dcb3928c4
yakuptokmakci <yakuptokmakci71@gmail.com>
2024-03-13 00:11:59 +0300

    Compare card and visibility is done

--- src/components/Compare.vue
+++ src/components/Compare.vue
@@ -44,7 +44,45 @@
           </button>
         </div>
       </div>
-      <div class="row" v-if="compare_cards"><h2>mal</h2></div>
+      <div class="row" v-if="compare_cards">
+        <v-card class="mx-auto bg-secondary" max-width="344">
+          <v-img
+            height="200px"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3rCVTROB4ZmRelcdj3kcnYoGvnNwesVbjAQ&usqp=CAU"
+            cover
+          ></v-img>
+
+          <v-card-title> According To Our Algorithm </v-card-title>
+
+          <v-card-subtitle> The statistical Winner is </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="orange-lighten-3" variant="text"> Explore </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show = !show"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                I'm a thing. But, like most politicians, he promised more than
+                he could deliver. You won't have time for sleeping, soldier, not
+                with all the bed making you'll be doing. Then we'll go with that
+                data file! Hey, you add a one and two zeros to that or we walk!
+                You're going to do his laundry? I've got to find a way to
+                escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </div>
     </div>
   </div>
 </template>
@@ -58,6 +96,7 @@ const isCompare = ref(true);
 const compare_cards = ref(false);
 const selectedPlayer1 = ref("");
 const selectedPlayer2 = ref("");
+const show = ref(false);
 
 const handleClick = () => {
   isCompare.value = !isCompare.value;
@@ -69,5 +108,6 @@ const handleCompare = () => {
 
 const RemoveCompare = () => {
   compare_cards.value = false;
+  isCompare.value = true;
 };
 </script>
